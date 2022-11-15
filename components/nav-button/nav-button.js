f3b86f4095408625413282ebc51a16edc20dee2d
Muhammed Basaran <mbasaran0293@gmail.com>
2022-11-15 16:24:26 +0100

    add pagination

---
+++ components/nav-button/nav-button.js
@@ -0,0 +1,8 @@
+/*const prevButton = document.querySelector('[data-js="button-prev"]');
+const nextButton = document.querySelector('[data-js="button-next"]');
+
+export function back() => {
+   prevButton.addEventListener("click", () => {
+  console.log("hallo");
+}); 
+}*/
